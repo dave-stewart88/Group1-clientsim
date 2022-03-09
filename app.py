b58c2d5d623407a62e1e2b404d541c746d30dc57
LukeHamill <98018208+LukeHamill@users.noreply.github.com>
2022-03-09 17:28:30 +0000

    Post code integration on website

--- app.py
+++ app.py
@@ -15,30 +15,21 @@ def index():
 
 @app.route('/hello', methods=['POST'])
 def hello():
-    name = request.form.get('name')
+    postCode = request.form.get('postCode')
     file = "./food-hygiene-data/belfast-all.csv"
     data = pd.read_csv(file, header=0, usecols=['BusinessName','PostCode', 'BusinessType','RatingValue'])
     data.set_index = (['BusinessName'])
     data.index.name = None
-    d=[""]
 
-    # postCode = request.form.get('postCode')
-    # print(type(data))
-    # business = data.loc[data.BusinessName]
-    # rating = data.loc[data.RatingValue]
-    # print(data.PostCode[0])
-    # for e in data.PostCode:
-    #     d += str(e).split()
-    #print(d[1][0],d[1][1],d[1][2])
-    information = data.loc[(data.RatingValue == '5')&(data['PostCode'].str.contains('BT9'))]
+    information = data.loc[(data.RatingValue == '5')&(data['PostCode'].str.contains(postCode))]
     print(type(information))
 
     print(information.to_html(classes=['BusinessName, RatingValue']))
 
     # print(rating)
-    if name:
-        print('Request for hello page received with name=%s' % name)
-        return render_template('hello.html', name=name, tables=[information.to_html(classes=['table table-dark'],index=False,columns=['BusinessName','PostCode','RatingValue'],justify='left')],
+    if postCode:
+        print('Request for hello page received with Post Code=%s' % postCode)
+        return render_template('hello.html', tables=[information.to_html(classes=['table table-dark'],index=False,columns=['BusinessName','PostCode','RatingValue'],justify='left')],
                                data=['na','Rating Table'])
     else:
         print('Request for hello page received with no name or blank name -- redirecting')
